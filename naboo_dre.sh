32b2b9ac768017718571aae1db07ac993db0b69a
Johnny Willemsen <jwillemsen@remedy.nl>
2020-11-09 08:31:22 +0100

    Added ACE 6.5.x part of the scoreboard
        * ace6.xml:
        * tao2.xml:
          Added.
    
        * index.xml:
        * naboo_dre.sh:

--- naboo_dre.sh
+++ naboo_dre.sh
@@ -16,7 +16,9 @@ git pull
 /usr/bin/perl ./scoreboard.pl -v -d $OUTPUTDIR -i $CONFIGDIR/index.xml  2>&1 &
 # Generate other pages!
 /usr/bin/perl ./scoreboard.pl -b -d $OUTPUTDIR -f $CONFIGDIR/ace.xml -o ace.html -r ace.rss 2>&1 &
+/usr/bin/perl ./scoreboard.pl -b -d $OUTPUTDIR -f $CONFIGDIR/ace6.xml -o ace6.html -r ace6.rss 2>&1 &
 /usr/bin/perl ./scoreboard.pl -b -d $OUTPUTDIR -f $CONFIGDIR/tao.xml -o tao.html -r tao.rss 2>&1  &
+/usr/bin/perl ./scoreboard.pl -b -d $OUTPUTDIR -f $CONFIGDIR/tao2.xml -o tao2.html -r tao2.rss 2>&1  &
 /usr/bin/perl ./scoreboard.pl -b -d $OUTPUTDIR -f $CONFIGDIR/ciao.xml -o ciao.html -r ciao.rss 2>&1  &
 /usr/bin/perl ./scoreboard.pl -b -d $OUTPUTDIR -f $CONFIGDIR/dds.xml -o dds.html -r dds.rss 2>&1  &
 
@@ -24,7 +26,7 @@ git pull
 ##testmatrix/update_scoreboard.sh 2>&1 &
 
 # Generate integrated pages!
-/usr/bin/perl ./scoreboard.pl -b -d /export/web/www/scoreboard -z -j $CONFIGDIR/ace.xml,$CONFIGDIR/tao.xml,$CONFIGDIR/ciao.xml,$CONFIGDIR/dds.xml 2>&1 &
+/usr/bin/perl ./scoreboard.pl -b -d /export/web/www/scoreboard -z -j $CONFIGDIR/ace.xml,$CONFIGDIR/ace6.xml,$CONFIGDIR/tao.xml,$CONFIGDIR/tao2.xml,$CONFIGDIR/ciao.xml,$CONFIGDIR/dds.xml 2>&1 &
 
 #Generate build matrix
 #/usr/bin/perl buildmatrix/buildmatrix.pl $CONFIGDIR/ace.xml $OUTPUTDIR 1 > /project/taotmp/scoreboard/buildmatrix/output.html 2> /tmp/build.out
